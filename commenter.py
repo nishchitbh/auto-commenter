82b1b7fb83902b413f84368ab06ff581f5497b36
mr0bn0xi0us <nbhellossss@gmail.com>
2024-05-27 17:58:42 +0545

    Added a better system instruction

--- commenter.py
+++ commenter.py
@@ -7,14 +7,6 @@ load_dotenv()
 
 
 def parse():
-    """Parses command line arguments.
-
-    Args:
-        None
-
-    Returns:
-        str: The filename provided as an argument.
-    """
     parser = argparse.ArgumentParser(description="Auto commenter")
     parser.add_argument("--file", type=str, required=True, help="File to be commented")
     args = parser.parse_args()
@@ -23,19 +15,12 @@ def parse():
 
 
 def commentor(filename):
-    """Adds comments to a Python code file using a language model.
 
-    Args:
-        filename (str): The path to the Python code file.
-
-    Returns:
-        None
-    """
     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
     genai.configure(api_key=GOOGLE_API_KEY)
     model = genai.GenerativeModel(
         model_name="gemini-1.5-flash",
-        system_instruction="You are a coding assistant that helps in commenting codes. The inputs will be python codes with no useful comments. Then, you should add comments to the code. Add comments about how the main logic of the code works, what are the inputs and outputs of functions, etc. DHowever, you don't have to add comments on what the libraries do. If the file is already well commented, don't touch anything and return the file as it is."
+        system_instruction="You are a coding assistant whose job is to add useful comments to the given code. You must begin by briefly describing the code's purpose (e.g., \"This code takes a human image as input and returns the gender of the person in the image by using Convolutional Neural Networks. The process involves reading the image, passing it through CNN, and generating output.\"). Then, add comments about the main logic of the code, complex lines within the code, detailing function arguments and returns, and providing context where necessary. Refrain from commenting on library functionalities. If the code is already well commented, don't alter the code and return the code as it is."
     )
     with open(filename, "r") as file:
         code = file.read()
@@ -46,14 +31,6 @@ def commentor(filename):
 
 
 def main():
-    """Main function that orchestrates the comment addition process.
-
-    Args:
-        None
-
-    Returns:
-        None
-    """
     filename = parse()
     commentor(filename)
 
